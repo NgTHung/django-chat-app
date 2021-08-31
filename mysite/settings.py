eb51b75a98319a7714d72600bde9a042990e9a4b
nguyen hung <tuanhungag1@gmail.com>
2021-08-31 12:31:12 +0700

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -129,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 
-STATICFILES_DIRS = [
-    str(os.path.join(BASE_DIR, "static")),
-]
+# STATICFILES_DIRS = [
+#     str(os.path.join(BASE_DIR, "static")),
+# ]
